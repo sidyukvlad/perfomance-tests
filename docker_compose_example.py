ffd722a7b51fdfe3ad9c7840b7aff49596873842
hodchenko <sidyukvlad@gmail.com>
2025-11-04 22:35:14 +0300

    Initial commit

---
+++ docker_compose_example.py
@@ -0,0 +1,6 @@
+from redis import Redis
+
+cache = Redis(host="redis", port=6379)
+cache.set("example", 5)
+print("AAA")
+print(int(cache.get("example")) ** 2)
---
+++ main.py
@@ -0,0 +1,5 @@
+def print_hi(name):
+    print(f"{name}")
+
+if __name__ == '__main__':
+    print_hi("Pycharm")
