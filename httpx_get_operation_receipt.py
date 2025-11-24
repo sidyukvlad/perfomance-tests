9dc5c795f353fb97ff2e51e6ef3aaa20214e2fbc
hodchenko <sidyukvlad@gmail.com>
2025-11-25 01:48:35 +0300

    7.2 Практикуемся в работе с HTTPX, Практикуемся в работе с HTTPX: получение чека по операции

---
+++ httpx_get_operation_receipt.py
@@ -0,0 +1,46 @@
+import time
+import httpx
+
+# Создание нового пользователя
+create_user_payload = {
+    "email": f"user.{time.time()}@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
+}
+create_user_response = httpx.post("http://localhost:8003/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+
+open_credit_card_account_payload = {
+    "userId": create_user_response_data["user"]["id"]
+}
+open_credit_card_account_response = httpx.post(
+    "http://localhost:8003/api/v1/accounts/open-credit-card-account",
+    json=open_credit_card_account_payload
+)
+open_credit_card_account_response_data = open_credit_card_account_response.json()
+
+make_purchase_operation_payload = {
+    "status": "IN_PROGRESS",
+    "amount": 77.99,
+    "category": "taxi",
+    "cardId": open_credit_card_account_response_data["account"]["cards"][0]["id"],
+    "accountId": open_credit_card_account_response_data["account"]["id"]
+}
+
+make_purchase_operation_response = httpx.post(
+    "http://localhost:8003/api/v1/operations/make-purchase-operation",
+    json=make_purchase_operation_payload
+)
+make_purchase_operation_response_data = make_purchase_operation_response.json()
+
+operation_receipt_response = httpx.get(
+    f"http://localhost:8003/api/v1/operations/operation-receipt/"
+    f"{make_purchase_operation_response_data["operation"]["id"]}"
+)
+
+operation_receipt_response_data = operation_receipt_response.json()
+
+print("Operation receipt response", operation_receipt_response_data)
+print("Operation receipt response status code", operation_receipt_response.status_code)
