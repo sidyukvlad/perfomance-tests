2ecc076c8ff70ac44d06312a1e6fa3fe859f6dea
hodchenko <sidyukvlad@gmail.com>
2025-12-01 23:40:47 +0300

    8.1 Знакомство с Pydantic, Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,45 @@
+# {
+#   "id": "string",
+#   "email": "user@example.com",
+#   "lastName": "string",
+#   "firstName": "string",
+#   "middleName": "string",
+#   "phoneNumber": "string"
+# }
+
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на создание пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    user: UserSchema
