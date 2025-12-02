ac69d2379ba2253aa18cfb6f4e22c7214358ccc9
hodchenko <sidyukvlad@gmail.com>
2025-12-02 19:32:15 +0300

    8.2 Применяем Pydantic в HTTP API клиентах, Практикуемся в применении Pydantic в API клиентах: документы

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,28 +1,10 @@
-from typing import TypedDict
-
 from clients.http.client import HTTPClient
 from httpx import Response
 from clients.http.gateway.client import build_gateway_http_client
-
-
-class DocumentDict(TypedDict):
-    url: str
-    document: str
-
-
-class GetTariffDocumentResponseDict(TypedDict):
-    """
-    Описание структуры ответа получения тарифа по счету.
-    """
-    tariff: DocumentDict
-
-
-class GetContractDocumentResponseDict(TypedDict):
-    """
-    Описание структуры ответа получения контракта по счету.
-    """
-    contract: DocumentDict
-
+from clients.http.gateway.documents.schema import (
+    GetTariffDocumentResponseSchema,
+    GetContractDocumentResponseSchema
+)
 
 class DocumentsGatewayHTTPClient(HTTPClient):
     """
@@ -46,13 +28,13 @@ class DocumentsGatewayHTTPClient(HTTPClient):
         """
         return self.get(f"/api/v1/documents/contract-document/{account_id}")
 
-    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseDict:
+    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseSchema:
         response = self.get_tariff_document_api(account_id)
-        return response.json()
+        return GetTariffDocumentResponseSchema.model_validate_json(response.text)
 
-    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseDict:
+    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseSchema:
         response = self.get_contract_document_api(account_id)
-        return response.json()
+        return GetContractDocumentResponseSchema.model_validate_json(response.text)
 
 
 def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
