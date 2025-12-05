29707ef00885dceba8aafca36271269b1b372cf3
hodchenko <sidyukvlad@gmail.com>
2025-12-05 11:54:48 +0300

    Реализуем gRPC API-клиент для сервиса UsersGatewayService внутри grpc-gateway

---
+++ clients/grpc/client.py
@@ -0,0 +1,22 @@
+from grpc import Channel
+import grpc.experimental.gevent as grpc_gevent
+
+
+grpc_gevent.init_gevent()
+
+
+class GRPCClient:
+    """
+    Базовый класс gRPC-клиента.
+
+    Этот класс хранит общий канал (Channel) для связи с gRPC-сервером.
+    От него будут наследоваться все остальные специфические клиенты.
+    """
+    def __init__(self, channel: Channel):
+        """
+        Конструктор базового клиента.
+
+        :param channel: gRPC-канал, через который происходит подключение к серверу.
+                        Обычно создаётся один раз и переиспользуется.
+        """
+        self.channel = channel
