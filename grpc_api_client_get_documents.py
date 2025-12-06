d8889244fddeceb920c78ceea099f58c24440f63
hodchenko <sidyukvlad@gmail.com>
2025-12-06 12:47:58 +0300

    9.5 Практикуемся в использовании gRPC API клиентов, Практика использования API-клиентов: получение документов по счету

--- grpc_api_client_get_documents.py
+++ grpc_api_client_get_documents.py
@@ -8,17 +8,17 @@ accounts_gateway_client = build_accounts_gateway_grpc_client()
 documents_gateway_client = build_documents_gateway_grpc_client()
 
 create_user_response = users_gateway_client.create_user()
-open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
+open_credit_card_account_response = accounts_gateway_client.open_credit_card_account(
     user_id = create_user_response.user.id
 )
 get_tariff_document_response = documents_gateway_client.get_tariff_document(
-    account_id=open_debit_card_account_response.account.id
+    account_id=open_credit_card_account_response.account.id
 )
 get_contract_document_response = documents_gateway_client.get_contract_document(
-    account_id=open_debit_card_account_response.account.id
+    account_id=open_credit_card_account_response.account.id
 )
 
-print(create_user_response)
-print(open_debit_card_account_response)
-print(get_tariff_document_response)
-print(get_contract_document_response)
+print("Create user response:", create_user_response)
+print("Open credit card account response:", open_credit_card_account_response)
+print("Get tariff document response:", get_tariff_document_response)
+print("Get contract document response:", get_contract_document_response)
