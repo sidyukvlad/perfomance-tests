754006e97b53d8b53faf7c0905f707aabac85f87
hodchenko <sidyukvlad@gmail.com>
2025-12-24 22:57:32 +0300

    11.2 Пишем сидинг-билдер

---
+++ locust_builder.py
@@ -0,0 +1,19 @@
+from seeds.builder import  build_grpc_seeds_builder
+from seeds.dumps import save_seeds_result, load_seeds_result
+from seeds.schema.plan import SeedsPlan, SeedUsersPlan, SeedCardsPlan, SeedAccountsPlan
+
+builder = build_grpc_seeds_builder()
+result = builder.build(
+    SeedsPlan(
+        users=SeedUsersPlan(
+            count=100,
+            credit_card_accounts=SeedAccountsPlan(
+                count=1,
+                physical_cards=SeedCardsPlan(count=1)
+            )
+        ),
+    )
+)
+
+save_seeds_result(result=result, scenario="test-scenario")
+print(load_seeds_result(scenario="test-scenario"))
